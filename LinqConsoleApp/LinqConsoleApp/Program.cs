34c13eaea7ba0c314d178a397031b10459351099
Thanh Cong Pham Trong <miniluxy99@gmail.com>
2020-05-08 21:38:36 +0200

    All

---
+++ LinqConsoleApp/LinqConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            //d.Przyklad1();    // OK
+            //d.Przyklad2();    // OK
+            //d.Przyklad3();    // OK
+            //d.Przyklad4();    // OK
+            //d.Przyklad5();    // OK
+            //d.Przyklad6();    // OK
+            //d.Przyklad7();    // OK
+            //d.Przyklad8();    // OK
+            //d.Przyklad9();    // OK
+            //d.Przyklad10Button_Click();   // OK
+            //d.Przyklad11();   // OK
+            //d.Przyklad12();   // OK?
+        }
+    }
+}
